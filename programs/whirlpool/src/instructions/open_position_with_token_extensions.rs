11ea2ba80b11b5a741615c509d2994e2cbc5395d
haowei-selini <haowei@selinicapital.com>
2025-12-10 20:18:25 +0000

    Resolve dependency conflicts

--- programs/whirlpool/src/instructions/open_position_with_token_extensions.rs
+++ programs/whirlpool/src/instructions/open_position_with_token_extensions.rs
@@ -9,7 +9,7 @@ use anchor_spl::token_2022::Token2022;
 use crate::constants::nft::whirlpool_nft_update_auth::ID as WP_NFT_UPDATE_AUTH;
 use crate::util::{
     initialize_position_mint_2022, initialize_position_token_account_2022,
-    initialize_token_metadata_extension, mint_position_token_2022_and_remove_authority,
+    mint_position_token_2022_and_remove_authority,
 };
 
 #[derive(Accounts)]
@@ -46,86 +46,4 @@ pub struct OpenPositionWithTokenExtensions<'info> {
     /// CHECK: checked via account constraints
     #[account(address = WP_NFT_UPDATE_AUTH)]
     pub metadata_update_auth: UncheckedAccount<'info>,
-}
-
-/*
-  Opens a new Whirlpool Position with Mint and TokenAccount owned by Token-2022.
-*/
-pub fn handler(
-    ctx: Context<OpenPositionWithTokenExtensions>,
-    tick_lower_index: i32,
-    tick_upper_index: i32,
-    with_token_metadata: bool,
-) -> Result<()> {
-    let whirlpool = &ctx.accounts.whirlpool;
-    let position_mint = &ctx.accounts.position_mint;
-    let position = &mut ctx.accounts.position;
-
-    let position_seeds = [
-        b"position".as_ref(),
-        position_mint.key.as_ref(),
-        &[ctx.bumps.position],
-    ];
-
-    collect_rent_for_ticks_in_position(
-        &ctx.accounts.funder,
-        position,
-        &ctx.accounts.system_program,
-    )?;
-
-    position.open_position(
-        whirlpool,
-        position_mint.key(),
-        tick_lower_index,
-        tick_upper_index,
-    )?;
-
-    let is_non_transferable_position_required = whirlpool.is_non_transferable_position_required();
-
-    initialize_position_mint_2022(
-        position_mint,
-        &ctx.accounts.funder,
-        position,
-        &ctx.accounts.system_program,
-        &ctx.accounts.token_2022_program,
-        with_token_metadata,
-        is_non_transferable_position_required,
-    )?;
-
-    if with_token_metadata {
-        let (name, symbol, uri) = build_position_token_metadata(position_mint, position, whirlpool);
-
-        initialize_token_metadata_extension(
-            name,
-            symbol,
-            uri,
-            position_mint,
-            position,
-            &ctx.accounts.metadata_update_auth,
-            &ctx.accounts.funder,
-            &ctx.accounts.system_program,
-            &ctx.accounts.token_2022_program,
-            &position_seeds,
-        )?;
-    }
-
-    initialize_position_token_account_2022(
-        &ctx.accounts.position_token_account,
-        position_mint,
-        &ctx.accounts.funder,
-        &ctx.accounts.owner,
-        &ctx.accounts.token_2022_program,
-        &ctx.accounts.system_program,
-        &ctx.accounts.associated_token_program,
-    )?;
-
-    mint_position_token_2022_and_remove_authority(
-        position,
-        position_mint,
-        &ctx.accounts.position_token_account,
-        &ctx.accounts.token_2022_program,
-        &position_seeds,
-    )?;
-
-    Ok(())
 }
